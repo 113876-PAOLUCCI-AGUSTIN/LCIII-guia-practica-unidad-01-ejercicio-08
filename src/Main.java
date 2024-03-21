2679451a62c8600d7cda6bc7b1adb286e5a15946
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-21 18:46:20 -0300

    Solucion 1: Ciclo For

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,20 @@
 * 8. Ingresar 10 números por teclado, informar su suma y promedio.
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Double sumaTotal = 0.0;
+        Integer cantNumerosIngresados = 10;
+
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < cantNumerosIngresados; i++) {
+            sumaTotal = sumaTotal + input.nextDouble();
+        }
+        Double  promedio = sumaTotal / cantNumerosIngresados;
+        System.out.println("La suma de los números es: "+sumaTotal);
+        System.out.println("Y el promedio: "+promedio);
     }
 }
